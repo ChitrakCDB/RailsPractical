77f930fd11094e4abe6ea8d4312559fed986759d
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-03-22 17:42:52 +0530

    feat: TE5-T90 add active record callback practical

--- app/models/student.rb
+++ app/models/student.rb
@@ -19,4 +19,46 @@ class Student < ApplicationRecord
       errors.add(:department, "can't be #{department}")
     end
   end
+
+  before_validation  :DOB_validated_before
+  after_validation  :DOB_validated
+  after_create  :show_create
+  after_update  :show_update
+  after_destroy :show_destroy, if: Proc.new{ |student| student.department == "IT"}
+  after_save  :show_save
+
+  after_initialize do |student|
+    puts "==>You have initialized an object in Student!"
+  end
+
+  after_find do |student|
+    puts "==>You have found an object in Student!"
+  end
+
+  private
+
+  def show_create
+    puts "==>Successfully created a record in Students"
+  end
+
+  def show_update
+    puts "==>Successfully updated a record in Students"
+  end
+
+  def show_destroy
+    puts "==>Successfully deleted a record in Students"
+  end
+
+  def show_save
+    puts "==>Successfully saved changes in Students"
+  end
+
+  def DOB_validated
+    puts "==>DOB validated Successfully in Students"
+  end
+
+  def DOB_validated_before
+    puts "==>DOB validating in Students"
+  end
+
 end
